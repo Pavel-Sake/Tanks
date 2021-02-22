edbecb76ba87cd2b0ab40a8780ac735358045f48
Pavel-Sake <53968568+Pavel-Sake@users.noreply.github.com>
2021-02-22 21:56:56 +0300

    feat: add dataRivalTankInSprite (#11)

--- dataTankInSprite/dataTankinSprite.js
+++ dataTankInSprite/dataTankinSprite.js
@@ -25,4 +25,31 @@ const dataTankInSprite = {
   }
 };
 
-export default dataTankInSprite;
+const dataRivalTankInSprite = {
+  positionUp: {
+    x: 525,
+    y: 981,
+    width: 134,
+    height: 195,
+  },
+  positionDown: {
+    x: 1,
+    y: 1846,
+    width: 134,
+    height: 195,
+  },
+  positionRight: {
+    x: 5,
+    y: 525,
+    width: 195,
+    height: 134,
+  },
+  positionLeft: {
+    x: 321,
+    y: 1182,
+    width: 195,
+    height: 134,
+  }
+};
+
+export {dataTankInSprite, dataRivalTankInSprite};
